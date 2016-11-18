073e9941ef645bee78286e90a7149dbe473cb0b1
Jesse R Weigel <jesse@jesseweigel.com>
2016-11-18 15:31:37 -0500

    Add sass

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
-  //Init scrollspy
-  $('.scrollspy').scrollSpy();
-});
