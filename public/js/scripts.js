91e1cdbae83d19c61cd6c0a636573c2f1ded2683
Jesse R Weigel <jesse@jesseweigel.com>
2016-11-18 15:43:48 -0500

    Ran eject

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+  //Init scrollspy
+  $('.scrollspy').scrollSpy();
+});
