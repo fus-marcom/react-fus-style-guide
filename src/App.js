ebb98ac30d71a345128488258705d68ccd22e320
Jesse R Weigel <jesse@jesseweigel.com>
2016-11-22 16:43:47 -0500

    Tested getting api data

--- src/App.js
+++ src/App.js
@@ -2,70 +2,150 @@ import React, { Component } from 'react';
 import Section from './Section';
 import './App.css';
 
-const sections = [
-  {
-    header: "Colors",
-    content: `
-    <p class="flow-text">
-      The primary colors, Green and Gold, should be the main colors used for headers and footers. The other colors in the can be used as accent colors for floating action buttons, call to action buttons, call to action banners, or content that you want to emphasize. Keep in mind that overuse of accent colors will diminish their ability to draw attention to a specific area of the page.
-    </p>
-     `,
-    code: `
-      $fus-yellow:     #ffc658;
-      $fus-light-blue: #8e9fbc;
-      $fus-dark-blue:  #1f2a44;
-      $fus-green:      #21412a;
-      $fus-gold:       #998643;
-      `
-  },
-  {
-    header: "Header 2",
-    content: "<h1>h1</h1>",
-    code: `
-      $fus-yellow:     #ffc658;
-
-      $fus-gold:       #998643;
-      `
-  },
-  {
-    header: "Header 3",
-    content: "Content 3",
-    code: `
-      $fus-yellow:     #ffc658;
-      $fus-light-blue: #8e9fbc;
-      $fus-dark-blue:  #1f2a44;
-      $fus-green:      #21412a;
-      $fus-gold:       #998643;
-      `
-  }
-];
-
+// let sections = [
+//   {
+//     header: "Colors",
+//     content: `
+//     <p class="flow-text">
+//       The primary colors, Green and Gold, should be the main colors used for headers and footers. The other colors in the can be used as accent colors for floating action buttons, call to action buttons, call to action banners, or content that you want to emphasize. Keep in mind that overuse of accent colors will diminish their ability to draw attention to a specific area of the page.
+//     </p>
+//      `,
+//     code: `
+//       $fus-yellow:     #ffc658;
+//       $fus-light-blue: #8e9fbc;
+//       $fus-dark-blue:  #1f2a44;
+//       $fus-green:      #21412a;
+//       $fus-gold:       #998643;
+//       `
+//   },
+//   {
+//     header: "Header 2",
+//     content: "<h1>h1</h1>",
+//     code: `
+//       $fus-yellow:     #ffc658;
+//
+//       $fus-gold:       #998643;
+//       `
+//   },
+//   {
+//     header: "Header 3",
+//     content: "Content 3",
+//     code: `
+//       $fus-yellow:     #ffc658;
+//       $fus-light-blue: #8e9fbc;
+//       $fus-dark-blue:  #1f2a44;
+//       $fus-green:      #21412a;
+//       $fus-gold:       #998643;
+//       `
+//   }
+// ];
 class App extends Component {
-  getInitialState() {
 
+  constructor(props) {
+		super(props);
+		this.state = {sections: [
+      {
+        title: {
+          rendered: "Colors"
+        },
+        content:  {
+
+          rendered:
+          `
+          <p class="flow-text">
+            The primary colors, Green and Gold, should be the main colors used for headers and footers. The other colors in the can be used as accent colors for floating action buttons, call to action buttons, call to action banners, or content that you want to emphasize. Keep in mind that overuse of accent colors will diminish their ability to draw attention to a specific area of the page.
+          </p>
+           `
+         },
+        code: `
+          $fus-yellow:     #ffc658;
+          $fus-light-blue: #8e9fbc;
+          $fus-dark-blue:  #1f2a44;
+          $fus-green:      #21412a;
+          $fus-gold:       #998643;
+          `
+      },
+      {
+        title: {
+          rendered: "Colors"
+        },
+        content:  {
+
+          rendered:
+          `
+          <p class="flow-text">
+            The primary colors, Green and Gold, should be the main colors used for headers and footers. The other colors in the can be used as accent colors for floating action buttons, call to action buttons, call to action banners, or content that you want to emphasize. Keep in mind that overuse of accent colors will diminish their ability to draw attention to a specific area of the page.
+          </p>
+           `
+         },
+        code: `
+          $fus-yellow:     #ffc658;
+          $fus-light-blue: #8e9fbc;
+          $fus-dark-blue:  #1f2a44;
+          $fus-green:      #21412a;
+          $fus-gold:       #998643;
+          `
+      },
+      {
+        title: {
+          rendered: "Colors"
+        },
+        content:  {
+
+          rendered:
+          `
+          <p class="flow-text">
+            The primary colors, Green and Gold, should be the main colors used for headers and footers. The other colors in the can be used as accent colors for floating action buttons, call to action buttons, call to action banners, or content that you want to emphasize. Keep in mind that overuse of accent colors will diminish their ability to draw attention to a specific area of the page.
+          </p>
+           `
+         },
+        code: `
+          $fus-yellow:     #ffc658;
+          $fus-light-blue: #8e9fbc;
+          $fus-dark-blue:  #1f2a44;
+          $fus-green:      #21412a;
+          $fus-gold:       #998643;
+          `
+      },
+    ]};
+	}
+
+  componentDidMount() {
+    // fetch(`https://franciscan.university/HfjGnJUHz77wyWm5S8/wp-json/wp/v2/posts?`)
+    //   .then(result=> {
+    //       this.setState({sections:result.json()});
+    //   })
+    //   .then(console.log(this.state.sections));
+
+    function get(url){
+      return fetch(url, {
+        method: 'get'
+      }).then(response => {
+        return response;
+      });
+    }
+
+
+
+    function getJSON(url) {
+    return get(url).then(function(response) {
+      return response.json();
+    });
+    }
+
+    getJSON(`https://franciscan.university/HfjGnJUHz77wyWm5S8/wp-json/wp/v2/posts?`)
+    .then(function(data) {
+      this.setState({sections:data});
+    });
   }
-
-  // componentDidMount() {
-  //   $.ajax({
-  //       url: `https://franciscan.university/HfjGnJUHz77wyWm5S8/wp-json/wp/v2/posts?`,
-  //       dataType: 'json',
-  //       cache: false,
-  //       success: function(data) {
-  //           this.setState({data: data});
-  //       }.bind(this),
-  //           error: function(xhr, status, err) {
-  //           console.error(this.props.url, status, err.toString());
-  //       }.bind(this)
-  //   });
-  // }
 
 
   render() {
     return (
       <div className="row">
         <div className="col s12 m9 l10">
-          {sections.map(function(section, i){
-            return <Section header={section.header} content={section.content} key={i} code={section.code}/>;
+          {this.state.sections.map(function(section, i){
+            return <Section header={section.title.rendered} content={section.content.rendered} key={i} code={section.code}/>;
           })}
         </div>
       </div>
