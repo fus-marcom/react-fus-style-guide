ef3bd490b27bb68c640f5796e187a3860dbaa98c
Jesse R Weigel <jesse@jesseweigel.com>
2016-12-16 16:33:14 -0500

    Add liscense to social icons

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ class App extends Component {
           </pre>
           <p class="promo-caption">Social Icons</p>
           <p class="flow-text">
-            Social media icons are added with the following tags.
+            Social media icons are added with the following tags. (Icons by Andreas Larsen: <a href="https://github.com/larsenwork/web.svg.min" title="web.svg.min GitHub Repo" target="_blank">https://github.com/larsenwork/web.svg.min</a>)
           </p>
           <div class="social">
   <a class="facebook">
@@ -290,6 +290,15 @@ class App extends Component {
      &lt;/svg>
   &lt;/a>
 &lt;/div>
+&lt;!--
+Copyright (c) 2016 by Andreas Larsen (http://codepen.io/larsenwork/pen/admEZM)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
 
             </code>
           </pre>
